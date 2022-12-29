274a45115c5fc4b6a34c3f0d6284990de46e15a0
amichele <alaiamichele04@gmail.com>
2022-12-29 11:02:29 +0100

    d

--- src/main/java/entity/connection/DbCon.java
+++ src/main/java/entity/connection/DbCon.java
@@ -15,7 +15,7 @@ public class DbCon {
 			try {
 				Class.forName("com.mysql.cj.jdbc.Driver");
 				connection=DriverManager.getConnection("jdbc:mysql://localhost:3306/dressapp","root","michele12");
-				System.out.print("connecteddddddddddddddddd");
+				System.out.print("connected");
 			} catch (ClassNotFoundException | SQLException e) {
 				e.printStackTrace();
 			}
