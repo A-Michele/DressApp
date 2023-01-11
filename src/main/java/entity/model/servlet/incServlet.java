2063ad032fe7460bf8578b5405f97150d0ba3a59
A-Michele <84576875+A-Michele@users.noreply.github.com>
2023-01-11 23:00:37 +0100

    Update incServlet.java

--- src/main/java/entity/model/servlet/incServlet.java
+++ src/main/java/entity/model/servlet/incServlet.java
@@ -3,6 +3,7 @@ package entity.model.servlet;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.sql.SQLException;
+import java.util.ArrayList;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -11,7 +12,10 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import entity.connection.DbCon;
-import entity.dao.CartDao;
+import entity.dao.CappelloDao;
+import entity.dao.DettaglioOrdineDAO;
+import entity.model.Cappello;
+import entity.model.DettaglioOrdine;
 import entity.model.User;
 
 @WebServlet("/inc")
@@ -23,11 +27,18 @@ public class incServlet extends HttpServlet {
 		try(PrintWriter out=response.getWriter()){
 			int p_id= Integer.parseInt(request.getParameter("p_id"));
 			User auth=(User) request.getSession().getAttribute("auth");
-			if(p_id>0) {
-				CartDao cDao=new CartDao(DbCon.getConnection());
-				cDao.inc(p_id,auth.getId());
+			int o_id=(int) request.getSession().getAttribute("o_id");
+			ArrayList<DettaglioOrdine> list=(ArrayList<DettaglioOrdine>) request.getSession().getAttribute("listDettaglio");
+			DettaglioOrdineDAO dtDao=new DettaglioOrdineDAO(DbCon.getConnection());
+			CappelloDao cdao=new CappelloDao(DbCon.getConnection());
+			Cappello c=cdao.retriveProductById(p_id);
+			if((dtDao.getQuantita(p_id, o_id)>=c.getDisp())){
+				getServletContext().setAttribute("errorIncrement",  "errorIncrement");
 				response.sendRedirect("cart.jsp");
-			}else {
+				return;
+			}
+			else {
+				dtDao.updateQuantita((dtDao.getQuantita(p_id, o_id))+1,p_id,o_id);	
 				response.sendRedirect("cart.jsp");
 			}
 		} catch (ClassNotFoundException e) {
