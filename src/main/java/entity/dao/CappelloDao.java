3ce47be64795d45b93539bad808cb94184aee16d
A-Michele <84576875+A-Michele@users.noreply.github.com>
2023-01-12 17:59:45 +0100

    Update CappelloDao.java

--- src/main/java/entity/dao/CappelloDao.java
+++ src/main/java/entity/dao/CappelloDao.java
@@ -26,7 +26,7 @@ public class CappelloDao {
 	public List<Cappello> getAllProducts(){
 		List<Cappello> products=new ArrayList<Cappello>();
 		try {
-			query="SELECT * FROM cappello";
+			query="SELECT * FROM cappello WHERE modificato=false";
 			//Connection c = DbCon.getConnection();
 			pst=this.con.prepareStatement(query);
 			rs=pst.executeQuery();
@@ -39,7 +39,7 @@ public class CappelloDao {
 				row.setFoto(rs.getString("foto"));
 				row.setDescrizione(rs.getString("descrizione"));
 				row.setDisp(rs.getInt("disponibilita"));
-				row.setDataUltimaModifica(rs.getDate("data_ultima_modifica"));
+				row.setModificato(rs.getBoolean("modificato"));
 				products.add(row);
 			}
 		}catch(Exception e) {
@@ -47,81 +47,13 @@ public class CappelloDao {
 		}
 		return products;
 	}
-
-	 /*public List<Cart> getCartProduct(ArrayList<Cart> cartList){
-		List<Cart> products=new ArrayList<Cart>();
-		try{
-			if(cartList.size()>0){
-				for(Cart item:cartList) {
-					query="SELECT * FROM prodotto WHERE id=?";
-					pst=this.con.prepareStatement(query);
-					pst.setInt(1,item.getId());
-					rs=pst.executeQuery();
-					while(rs.next()) {
-						Cart row=new Cart();
-						row.setId(rs.getInt("id"));
-						row.setNome(rs.getString("nome"));
-						row.setCategoria(rs.getString("categoria"));
-						row.setPrezzo(rs.getDouble("costo")*item.getQuantita());
-						row.setQuantita(item.getQuantita());
-						
-						products.add(row);
-					}
-				}
-			}
-		}catch(Exception e) {
-			System.out.println(e.getMessage());
-			//e.printStacktrace();
-		}
-		return products;
-	}  
-	
-	public double getTotal(ArrayList<Cart> cartList) {
-		double sum=0;
-		try {
-			if(cartList.size()>0) {
-				for(Cart item: cartList) {
-					query="SELECT prezzo FROM cappello WHERE id=?";
-					pst=this.con.prepareStatement(query);
-					pst.setInt(1,item.getId_prodotto());
-					rs=pst.executeQuery();
-					while(rs.next()) {
-						sum+=rs.getDouble("costo")*item.getQuantita();
-					}
-				}
-			}
-		}catch(Exception e) {
-			e.printStackTrace();
-		}
-		return sum;
-	}
-	
-	public Cart completeCart(Cart c) {
-		query="SELECT nome,categoria,costo FROM prodotto WHERE id=?";
-		try {
-			pst=this.con.prepareStatement(query);
-			pst.setInt(1,c.getId_prodotto());
-			rs=pst.executeQuery();
-			while(rs.next()) {
-				c.setNome(rs.getString("nome"));
-				c.setCategoria(rs.getString("categoria"));
-				c.setPrezzo(rs.getDouble("costo"));
-			}
-		}catch(Exception e) {
-			e.printStackTrace();
-		}
-		return c;
-	}
-	 
-	 */
-	 
 	//------------------------------------
 	// Metodi CRUD:
 	
 	public ArrayList<Cappello> searchItems(String s){
         ArrayList<Cappello> products=new ArrayList<Cappello>();
         try {
-            query="SELECT * FROM Cappello WHERE nome LIKE '%"+s+"%'";
+            query="SELECT * FROM Cappello WHERE nome LIKE '%"+s+"%' AND modificato=false";
             pst=con.prepareStatement(query);
             rs=pst.executeQuery();
             while(rs.next()) {
@@ -133,7 +65,7 @@ public class CappelloDao {
                 p.setCategoria(rs.getString("categoria"));
                 p.setFoto(rs.getString("foto"));
                 p.setDisp(rs.getInt("disponibilita"));
-                p.setDataUltimaModifica(rs.getDate("data_ultima_modifica"));
+                p.setModificato(rs.getBoolean("modificato"));
                 products.add(p);
             }
         }catch(Exception e) {
@@ -155,9 +87,8 @@ public class CappelloDao {
 	}
 	
 	public void insertProduct(String nome,String descrizione,float costo,String categoria,String foto, int disp) {
-		Cappello p= new Cappello(); // Creo il cappello per fargli generare automaticamente l'id corretto da inserire nella query
         try {
-            query="INSERT INTO Cappello(id,nome,descrizione,prezzo,categoria,foto,disponibilita,data_ultima_modifica)"
+            query="INSERT INTO Cappello(id,nome,descrizione,prezzo,categoria,foto,disponibilita,modificato)"
             		+ " VALUES(?,?,?,?,?,?,?,?)";
             pst=this.con.prepareStatement(query);
             pst.setInt(1, 0);
@@ -167,7 +98,7 @@ public class CappelloDao {
             pst.setString(5, categoria);
             pst.setString(6, foto);
             pst.setInt(7, disp);
-            pst.setDate(8, new Date(System.currentTimeMillis()));
+            pst.setBoolean(8, false);
             pst.executeUpdate();
         }
         catch(Exception e){
@@ -191,7 +122,7 @@ public class CappelloDao {
             	p.setCategoria(rs.getString("categoria"));
             	p.setFoto(rs.getString("foto"));
             	p.setDisp(rs.getInt("disponibilita"));
-            	p.setDataUltimaModifica(rs.getDate("data_ultima_modifica"));
+            	p.setModificato(rs.getBoolean("modificato"));
             }
             return p;
 			
@@ -202,9 +133,9 @@ public class CappelloDao {
 	}
 
 	public boolean updateProdotto(int id, String nome, String desc, float costo, String categoria, String foto,
-			int dispo, Date data) {
+			int dispo, Boolean modificato) {
 		try {
-            query="UPDATE Cappello SET nome=?, descrizione=?, prezzo=?, categoria=?,foto=?,disponibilita=?,data_ultima_modifica=? WHERE id=?";
+            query="UPDATE Cappello SET nome=?, descrizione=?, prezzo=?, categoria=?,foto=?,disponibilita=?,modificato=? WHERE id=?";
             pst=this.con.prepareStatement(query);
             pst.setString(1, nome);
             pst.setString(2, desc);
@@ -212,7 +143,7 @@ public class CappelloDao {
             pst.setString(4, categoria);
             pst.setString(5, foto);
             pst.setInt(6, dispo);
-            pst.setDate(7, data);
+            pst.setBoolean(7, modificato);
             pst.setInt(8, id);
             pst.executeUpdate();
             return true;
@@ -234,5 +165,16 @@ public class CappelloDao {
             e.printStackTrace();
         }
 		
+	}
+	
+	public void updateModificato(int id) {
+		try {
+            query="UPDATE Cappello SET modificato=true WHERE id=?";
+            pst=this.con.prepareStatement(query);
+            pst.setInt(1, id);
+            pst.executeUpdate();
+        }catch(Exception e){
+            e.printStackTrace();
+        }
 	}
 }
