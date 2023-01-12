e8bd70364fc225b979482df96faec02ba3c3ca50
A-Michele <84576875+A-Michele@users.noreply.github.com>
2023-01-12 18:54:48 +0100

    Update selectPayment.java

--- src/main/java/entity/model/servlet/selectPayment.java
+++ src/main/java/entity/model/servlet/selectPayment.java
@@ -2,6 +2,7 @@ package entity.model.servlet;
 
 import java.io.IOException;
 import java.sql.SQLException;
+import java.util.List;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -10,7 +11,11 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import entity.connection.DbCon;
+import entity.dao.CappelloDao;
+import entity.dao.DettaglioOrdineDAO;
 import entity.dao.OrderDao;
+import entity.model.Cappello;
+import entity.model.DettaglioOrdine;
 import entity.model.Ordine;
 
 /**
@@ -35,6 +40,16 @@ public class selectPayment extends HttpServlet {
 		try {
 			int o_id = Integer.parseInt(request.getParameter("o_id"));
 			OrderDao oDao = new OrderDao(DbCon.getConnection());
+			DettaglioOrdineDAO dDao =new DettaglioOrdineDAO(DbCon.getConnection());
+			List<DettaglioOrdine> list=dDao.searchDettaglioOrdineByOrdineId(o_id);
+			CappelloDao cDao=new CappelloDao(DbCon.getConnection());
+			for(DettaglioOrdine o :list) {
+				int p_id=o.getCappello();
+				int quantita=o.getQuantita();
+				Cappello c=cDao.retriveProductById(p_id);
+				cDao.riduciDisponibilita(p_id,c.getDisp()-quantita);
+			}
+			
 			boolean x = oDao.changeState(o_id);
 			if(x==true) {
 				response.sendRedirect("cart.jsp");
